6ba4d901cba61aed757de9850a4a2b12933c00a1
kimxxunu <sam11222@naver.com>
2024-11-09 03:11:32 +0900

    [#5] Test : 기본적인 CRUD 테스트 성공 성공 Get, Post 둘다 잘됨

---
+++ src/user/user.schema.ts
@@ -0,0 +1,14 @@
+// src/user/user.schema.ts
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class User extends Document {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop()
+  email: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
