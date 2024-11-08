4f043fc0c8dbf91da4bb27d9bd16e0f3bd76e70b
kimxxunu <sam11222@naver.com>
2024-11-09 03:27:29 +0900

    [#5] Feat : MongoDB 연결완료 이슈 해결

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
-import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGODB_URI), 
-    UserModule, 
   ],
   controllers: [AppController],
   providers: [AppService],
