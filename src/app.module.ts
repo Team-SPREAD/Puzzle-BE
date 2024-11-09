a8c0f235a82e000b1314dde16ec14255167fb846
kimxxunu <sam11222@naver.com>
2024-11-10 02:45:12 +0900

    [#7] Feat : 구글 로그인 기능 구현 완료 app.module 에 기능 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './google-oauth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.MONGODB_URI), 
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    AuthModule, 
+    UsersModule
   ],
   controllers: [AppController],
   providers: [AppService],
-  
 })
 export class AppModule {}
