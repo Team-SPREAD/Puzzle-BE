9f03c109b2190c451f0f2e1a6c6f8fd070dc437f
SUNNY_KIM <114386406+Kimxxunu@users.noreply.github.com>
2024-11-10 03:30:53 +0900

    Swagger 연결 (#10)
    * [#8] Chore : Swagger 관련 패키지 설치
    swagger관련 패키지 설치
    
    * [#8] Chore : Swagger 연동완료
    swagger 연동 및  테스트 완료

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Swagger Example')
+    .setDescription('Swagger study API description')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .addTag('swagger')
+    .build();
+
+  // config를 바탕으로 swagger document 생성
+  const document = SwaggerModule.createDocument(app, config);
+  // Swagger UI에 대한 path를 연결함
+  SwaggerModule.setup('api', app, document);
+
+  // CORS 설정
+  app.enableCors();
+
+  await app.listen(3000);
 }
 bootstrap();
