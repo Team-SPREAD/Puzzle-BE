78ba217c3a12bb1527f99e43f9f8f04c5793f15c
SUNNY_KIM <114386406+Kimxxunu@users.noreply.github.com>
2024-11-10 22:35:36 +0900

    Swagger 수정 (#14)
    * [#13] Fix : Swagger 전반적인 기능 수정함
    각 API에 대한 설명 기입 및 토큰 인증 기능 활성화함,(토큰 기입 할때 'Bearer '제외하고 토큰 자체만 기입해야함)
    
    * [#13] Fix : Swagger에서 DTO 정보 확인 할 수 있게함
    TeamDto swagger에서 보이게 설정함

--- src/teams/teams.controller.ts
+++ src/teams/teams.controller.ts
@@ -1,31 +1,36 @@
 // src/teams/team.controller.ts
-import { Controller, Get, Post, Body, Req, UseGuards, Param } from '@nestjs/common'; // Param 추가
+import { Controller, Get, Post, Body, Req, UseGuards, Param } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { TeamDto } from './teams.dto';
 import { Team } from './teams.schema';
 import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
 import { TeamService } from './teams.service';
 
+@ApiTags('Teams') // API 태그 추가
+@ApiBearerAuth()
 @Controller('teams')
 export class TeamController {
   constructor(private readonly teamService: TeamService) {}
 
   @Post()
   @UseGuards(AuthGuard('jwt'))
+  @ApiOperation({ summary: '팀 생성', description: '새로운 팀을 생성합니다.' })
   async createTeam(@Body() teamDto: TeamDto, @Req() req: Request): Promise<Team> {
     const userId = req.user['id'];
     return this.teamService.createTeam(teamDto, userId);
   }
 
-  // 현재 로그인된 사용자가 속해 있는 모든 팀을 불러오는 엔드포인트
   @Get('my-teams')
   @UseGuards(AuthGuard('jwt'))
+  @ApiOperation({ summary: '사용자의 팀 조회', description: '로그인된 사용자가 속한 팀들을 조회합니다.' })
   async getMyTeams(@Req() req: Request): Promise<Team[]> {
     const userId = req.user['id'];
     return this.teamService.findTeamsByUserId(userId);
   }
 
   @Get(':id/users')
+  @ApiOperation({ summary: '팀 사용자 조회', description: '특정 팀에 속한 모든 사용자를 조회합니다.' })
   async getTeamUsers(@Param('id') teamId: string) {
     return this.teamService.getTeamUsers(teamId);
   }
--- src/teams/teams.dto.ts
+++ src/teams/teams.dto.ts
@@ -1,5 +1,10 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class TeamDto {
+
+    @ApiProperty()
     readonly teamName: string;
+    @ApiProperty()
     readonly users?: string[];
   }
   
