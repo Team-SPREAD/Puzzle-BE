41e34b8e845dc52c94c0d15e10364cab8fe5834b
kimxxunu <sam11222@naver.com>
2024-12-02 04:43:13 +0900

    [#38] Feat : HealthCheck APi 추가 healthCheck API 개발

---
+++ src/global/healthcheck.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Controller('api')
+export class HealthCheckController {
+  constructor(private readonly configService: ConfigService) {}
+
+  // 서버 환경 상태를 반환하는 엔드포인트
+  @Get('hc')
+  healthCheck() {
+    const serverName = this.configService.get<string>('serverName');
+    const serverPort = this.configService.get<string>('server.port');
+    const env = this.configService.get<string>('server.env');
+
+    // 서버 상태 정보를 반환
+    return {
+      serverName,
+      serverPort,
+      env,
+    };
+  }
+
+  // 서버 환경을 반환하는 엔드포인트
+  @Get('env')
+  getEnv() {
+    const env = this.configService.get<string>('server.env');
+    return { env };
+  }
+}
